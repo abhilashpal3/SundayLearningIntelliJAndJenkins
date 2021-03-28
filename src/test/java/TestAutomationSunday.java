4a587f854ed6c7e0047383328c7eceaf7edc5150
abhilashpal3@gmail.com <abhilashpal3@gmail.com>
2021-03-28 12:40:06 +0530

    Learn basics sunday 2

--- src/test/java/TestAutomationSunday.java
+++ src/test/java/TestAutomationSunday.java
@@ -1,3 +1,4 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -10,12 +11,15 @@ public class TestAutomationSunday {
     @Test
     public void launchBrowserAndHitURl()
     {
-        System.setProperty("webdriver.chrome.driver",".//Drivers//chromedriver.exe");
+     //   System.setProperty("webdriver.chrome.driver",".//Drivers//chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
         WebDriver driver= new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         // driver.get("https://www.facebook.com/");
         driver.navigate().to("https://www.facebook.com/");
+
+        System.out.println("All code executed");
 
     }
 }
