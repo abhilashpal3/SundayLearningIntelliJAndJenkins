226c2e98b6eaba696b4b1c0edc38997fea0d53a4
abhilashpal3@gmail.com <abhilashpal3@gmail.com>
2021-03-28 12:12:47 +0530

    Learn basics sunday

---
+++ src/test/java/TestAutomationSunday.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestAutomationSunday {
+
+
+    @Test
+    public void launchBrowserAndHitURl()
+    {
+        System.setProperty("webdriver.chrome.driver",".//Drivers//chromedriver.exe");
+        WebDriver driver= new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        // driver.get("https://www.facebook.com/");
+        driver.navigate().to("https://www.facebook.com/");
+
+    }
+}
